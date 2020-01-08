f30ed7b1ad7dddea80380ffb726aec2cdf3890fb
ADem-kt <59634979+ADem-kt@users.noreply.github.com>
2020-01-08 07:12:08 +0300

    Add files via upload

---
+++ ProjecNIR.dpr
@@ -0,0 +1,14 @@
+program ProjecNIR;
+
+uses
+  Forms,
+  Unittttttttttttttttttttttttt2 in '..\Õ»–76\Unittttttttttttttttttttttttt2.pas' {Form1},
+  Unit2 in '..\Õ»–76\Unit2.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+  end.
